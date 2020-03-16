def8141e85d4b903bf6d527608a96ebd656a1b62
Martijn Hartlief <martijn@keicreations.nl>
2020-03-16 17:45:03 +0100

    Bearer polyfill for mercure

--- store/index.js
+++ store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import user from './modules/user';
 import mercure from './modules/mercure';
+import api from './../lib/api-platform'
 
 Vue.use(Vuex);
 
@@ -10,4 +11,26 @@ export default new Vuex.Store({
         user,
         mercure,
     },
+    actions: {
+        login(context, username, password) {
+            return new Promise(function (resolve, reject) {
+                api.anonymous().post('/authentication_token', {
+                    email: username,
+                    password: password
+                }).then(response => {
+                    context.commit('user/setToken', response.data.token);
+                    context.commit('user/setRefreshToken', response.data.refresh_token);
+                    context.commit('mercure/setToken', response.data.mercure_token);
+
+                    resolve();
+                }).catch(reason => {
+                    context.commit('user/setToken', null);
+                    context.commit('user/setRefreshToken', null);
+                    context.commit('mercure/setToken', null);
+
+                    reject(reason.response);
+                });
+            });
+        }
+    }
 });
