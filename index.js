32010b5776021ac1b199466089422b1344288b25
jordi <jordisipkens@gmail.com>
2020-12-22 10:24:00 +0100

    User module kan je nu meesturen, zodat je bij KeiPOS gewoon je eigen user store kunt gebruiken.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import ResourceGrid from "./components/resource/ResourceGrid";
 import ResourceCollection from "./components/resource/ResourceCollection";
 import ResourceItem from "./components/resource/ResourceItem";
 import ResourceList from "./components/resource/ResourceList";
+import store from "./store/index";
+import user from "./store/modules/user";
 
 export default {
     install(Vue, options) {
@@ -12,5 +14,11 @@ export default {
         Vue.component('ResourceCollections', ResourceCollection);
 
         Vue.prototype.$api = (options && options.api) ? options.api : api;
+
+        if(options && options.module && options.module.user){
+            store.registerModule('user', options.module.user);
+        } else {
+            store.registerModule('user', user)
+        }
     }
 }
